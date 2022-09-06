c1b14b5f73568e0d6c552b21d0822262461a7653
Wallace Tácio <wallacett.dev@gmail.com>
2022-09-06 14:57:55 -0300

    Adiciona solução ao exercicio area do circulo

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double raio, area, diametro, perimetro;
+
+Console.Write("Digite o valor do raio do circulo: ");
+
+raio = Convert.ToDouble(Console.ReadLine());
+
+area = Math.PI*Math.Pow(raio,2);
+diametro = 2*raio;
+perimetro = 2*Math.PI*raio;
+
+Console.WriteLine($"A área é igual a: {area:N2}\nO diamentro é igual a: {diametro:N2} \nO perimetro é igual a: {perimetro:N2}");
